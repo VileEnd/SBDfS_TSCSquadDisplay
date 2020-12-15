8981591cec67051f99745e30cfa035a8737a2c94
VileEnd <jan_kretschmer@protonmail.com>
2020-12-15 23:11:28 +0100

    get bottoken

--- app.ts
+++ app.ts
@@ -1,17 +1,17 @@
 import { Client } from '@typeit/discord';
-
+import {configBot} from './data';
 
 async function start(): Promise<void> {
   const client = new Client({
     classes: [
-      `${bot.ts}/*Discord.ts`, // glob string to load the classes
-      `${bot.js}/*Discord.js` // If you compile using "tsc" the file extension change to .js
+      `${bot.ts}/*Discord.ts`, 
+      `${bot.js}/*Discord.js` 
     ],
     silent: false,
     variablesChar: ":"
   });
 
-  await client.login("YOUR_TOKEN");
+  await client.login(configBot[1]);
 }
 
 start();
---
+++ data.ts
@@ -0,0 +1,5 @@
+let configBot = {
+  bottoken: '334324023075987_59042nz529t4234',
+}
+
+export { configBot };
